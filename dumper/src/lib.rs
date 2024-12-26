7ac1660399e3f81199fc163cbfe9f08249868701
Truman Kilen <t@kilen.me>
2024-12-26 12:36:06 -0600

    restructure

---
+++ dumper/src/lib.rs
@@ -0,0 +1,359 @@
+mod containers;
+
+use std::collections::BTreeMap;
+
+use anyhow::Result;
+use bitflags::Flags;
+use patternsleuth::resolvers::impl_try_collector;
+use read_process_memory::{CopyAddress as _, Pid, ProcessHandle};
+use serde::Serialize;
+use ue_reflection::{
+    Class, EClassCastFlags, EClassFlags, EFunctionFlags, EStructFlags, Enum, Function, ObjectType,
+    Property, PropertyType, Struct,
+};
+
+use crate::containers::{
+    ExternalPtr, FArrayProperty, FBoolProperty, FByteProperty, FEnumProperty, FField,
+    FInterfaceProperty, FLazyObjectProperty, FMapProperty, FObjectProperty, FProperty,
+    FSetProperty, FSoftObjectProperty, FStructProperty, FUObjectArray, FWeakObjectProperty, Mem,
+    MemCache, PtrFNamePool, TTuple, UClass, UEnum, UFunction, UObject, UScriptStruct, UStruct,
+};
+
+impl_try_collector! {
+    #[derive(Debug, PartialEq, Clone)]
+    struct DrgResolution {
+        guobject_array: patternsleuth::resolvers::unreal::guobject_array::GUObjectArray,
+        fname_pool: patternsleuth::resolvers::unreal::fname::FNamePool,
+    }
+}
+impl Mem for ProcessHandle {
+    fn read_buf(&self, address: usize, buf: &mut [u8]) -> Result<()> {
+        self.copy_address(address, buf)?;
+        Ok(())
+    }
+}
+
+struct Ctx<M: Mem> {
+    mem: M,
+    fnamepool: PtrFNamePool,
+}
+impl<M: Mem> Mem for Ctx<M> {
+    fn read_buf(&self, address: usize, buf: &mut [u8]) -> Result<()> {
+        self.mem.read_buf(address, buf)
+    }
+}
+
+// TODO
+// [ ] UEnum
+// [ ] UScriptStruct
+// [ ] UStruct?
+// [ ] parent clasess
+// [ ] interfaces
+// [ ] functions
+// [ ] functions signatures
+// [ ] native function pointers
+// [ ] dynamic structs
+// [ ] ue version info
+
+fn read_path<M: Mem>(mem: &Ctx<M>, obj: &UObject) -> Result<String> {
+    let mut components = vec![];
+    let name = mem.fnamepool.read(mem, obj.NamePrivate)?;
+
+    let mut outer = obj.OuterPrivate;
+    components.push(name);
+    while let Some(o) = outer.read_opt(mem)? {
+        components.push(mem.fnamepool.read(mem, o.NamePrivate)?);
+        outer = o.OuterPrivate;
+    }
+    components.reverse();
+    Ok(components.join("."))
+}
+
+fn map_prop<M: Mem>(mem: &Ctx<M>, ptr: ExternalPtr<FField>) -> Result<Option<Property>> {
+    let field = ptr.read(mem)?;
+    let name = mem.fnamepool.read(mem, field.NamePrivate)?;
+    let field_class = field.ClassPrivate.read(mem)?;
+    let f = field_class.CastFlags;
+
+    if !f.contains(EClassCastFlags::CASTCLASS_FProperty) {
+        return Ok(None);
+    }
+
+    let t = if f.contains(EClassCastFlags::CASTCLASS_FStructProperty) {
+        let prop = ptr.cast::<FStructProperty>().read(mem)?;
+        let s = read_path(
+            mem,
+            &prop.struct_.read_opt(mem)?.unwrap().ustruct.ufield.uobject,
+        )?;
+        PropertyType::Struct { r#struct: s }
+    } else if f.contains(EClassCastFlags::CASTCLASS_FStrProperty) {
+        PropertyType::Str
+    } else if f.contains(EClassCastFlags::CASTCLASS_FNameProperty) {
+        PropertyType::Name
+    } else if f.contains(EClassCastFlags::CASTCLASS_FTextProperty) {
+        PropertyType::Text
+    } else if f.contains(EClassCastFlags::CASTCLASS_FMulticastInlineDelegateProperty) {
+        // TODO function signature
+        PropertyType::MulticastInlineDelegate
+    } else if f.contains(EClassCastFlags::CASTCLASS_FMulticastSparseDelegateProperty) {
+        // TODO function signature
+        PropertyType::MulticastSparseDelegate
+    } else if f.contains(EClassCastFlags::CASTCLASS_FDelegateProperty) {
+        // TODO function signature
+        PropertyType::Delegate
+    } else if f.contains(EClassCastFlags::CASTCLASS_FBoolProperty) {
+        let prop = ptr.cast::<FBoolProperty>().read(mem)?;
+        PropertyType::Bool {
+            field_size: prop.FieldSize,
+            byte_offset: prop.ByteOffset,
+            byte_mask: prop.ByteMask,
+            field_mask: prop.FieldMask,
+        }
+    } else if f.contains(EClassCastFlags::CASTCLASS_FArrayProperty) {
+        let prop = ptr.cast::<FArrayProperty>().read(mem)?;
+        PropertyType::Array {
+            inner: map_prop(mem, prop.inner.cast())?.unwrap().r#type.into(),
+        }
+    } else if f.contains(EClassCastFlags::CASTCLASS_FEnumProperty) {
+        let prop = ptr.cast::<FEnumProperty>().read(mem)?;
+        PropertyType::Enum {
+            container: map_prop(mem, prop.underlying_prop.cast())?
+                .unwrap()
+                .r#type
+                .into(),
+            r#enum: read_path(mem, &prop.enum_.read(mem)?.ufield.uobject)?,
+        }
+    } else if f.contains(EClassCastFlags::CASTCLASS_FMapProperty) {
+        let prop = ptr.cast::<FMapProperty>().read(mem)?;
+        PropertyType::Map {
+            key_prop: map_prop(mem, prop.key_prop.cast())?.unwrap().r#type.into(),
+            value_prop: map_prop(mem, prop.value_prop.cast())?
+                .unwrap()
+                .r#type
+                .into(),
+        }
+    } else if f.contains(EClassCastFlags::CASTCLASS_FSetProperty) {
+        let prop = ptr.cast::<FSetProperty>().read(mem)?;
+        PropertyType::Set {
+            key_prop: map_prop(mem, prop.element_prop.cast())?
+                .unwrap()
+                .r#type
+                .into(),
+        }
+    } else if f.contains(EClassCastFlags::CASTCLASS_FFloatProperty) {
+        PropertyType::Float
+    } else if f.contains(EClassCastFlags::CASTCLASS_FDoubleProperty) {
+        PropertyType::Double
+    } else if f.contains(EClassCastFlags::CASTCLASS_FByteProperty) {
+        let prop = ptr.cast::<FByteProperty>().read(mem)?;
+        PropertyType::Byte {
+            r#enum: prop
+                .enum_
+                .read_opt(mem)?
+                .map(|e| read_path(mem, &e.ufield.uobject))
+                .transpose()?,
+        }
+    } else if f.contains(EClassCastFlags::CASTCLASS_FUInt16Property) {
+        PropertyType::UInt16
+    } else if f.contains(EClassCastFlags::CASTCLASS_FUInt32Property) {
+        PropertyType::UInt32
+    } else if f.contains(EClassCastFlags::CASTCLASS_FUInt64Property) {
+        PropertyType::UInt64
+    } else if f.contains(EClassCastFlags::CASTCLASS_FInt8Property) {
+        PropertyType::Int8
+    } else if f.contains(EClassCastFlags::CASTCLASS_FInt16Property) {
+        PropertyType::Int16
+    } else if f.contains(EClassCastFlags::CASTCLASS_FIntProperty) {
+        PropertyType::Int
+    } else if f.contains(EClassCastFlags::CASTCLASS_FInt64Property) {
+        PropertyType::Int64
+    } else if f.contains(EClassCastFlags::CASTCLASS_FObjectProperty) {
+        let prop = ptr.cast::<FObjectProperty>().read(mem)?;
+        let c = read_path(
+            mem,
+            &prop
+                .property_class
+                .read_opt(mem)?
+                .unwrap()
+                .ustruct
+                .ufield
+                .uobject,
+        )?;
+        PropertyType::Object { class: c }
+    } else if f.contains(EClassCastFlags::CASTCLASS_FWeakObjectProperty) {
+        let prop = ptr.cast::<FWeakObjectProperty>().read(mem)?;
+        let c = read_path(
+            mem,
+            &prop
+                .property_class
+                .read_opt(mem)?
+                .unwrap()
+                .ustruct
+                .ufield
+                .uobject,
+        )?;
+        PropertyType::WeakObject { class: c }
+    } else if f.contains(EClassCastFlags::CASTCLASS_FSoftObjectProperty) {
+        let prop = ptr.cast::<FSoftObjectProperty>().read(mem)?;
+        let c = read_path(
+            mem,
+            &prop
+                .property_class
+                .read_opt(mem)?
+                .unwrap()
+                .ustruct
+                .ufield
+                .uobject,
+        )?;
+        PropertyType::SoftObject { class: c }
+    } else if f.contains(EClassCastFlags::CASTCLASS_FLazyObjectProperty) {
+        let prop = ptr.cast::<FLazyObjectProperty>().read(mem)?;
+        let c = read_path(
+            mem,
+            &prop
+                .property_class
+                .read_opt(mem)?
+                .unwrap()
+                .ustruct
+                .ufield
+                .uobject,
+        )?;
+        PropertyType::LazyObject { class: c }
+    } else if f.contains(EClassCastFlags::CASTCLASS_FInterfaceProperty) {
+        let prop = ptr.cast::<FInterfaceProperty>().read(mem)?;
+        let c = read_path(
+            mem,
+            &prop
+                .interface_class
+                .read_opt(mem)?
+                .unwrap()
+                .ustruct
+                .ufield
+                .uobject,
+        )?;
+        PropertyType::Interface { class: c }
+    } else if f.contains(EClassCastFlags::CASTCLASS_FFieldPathProperty) {
+        // TODO
+        PropertyType::FieldPath
+    } else {
+        unimplemented!("{f:?}");
+    };
+
+    let prop = ptr.cast::<FProperty>().read(mem)?;
+    Ok(Some(Property {
+        name,
+        offset: prop.Offset_Internal as usize,
+        size: prop.ElementSize as usize,
+        flags: prop.PropertyFlags,
+        r#type: t,
+    }))
+}
+
+pub fn dump(pid: i32) -> Result<()> {
+    let mem: ProcessHandle = (pid as Pid).try_into()?;
+    let mem = MemCache::wrap(mem);
+
+    let results = patternsleuth::process::external::read_image_from_pid(pid)?
+        .resolve(DrgResolution::resolver())?;
+
+    let guobjectarray = ExternalPtr::<FUObjectArray>::new(results.guobject_array.0);
+    let fnamepool = PtrFNamePool(results.fname_pool.0);
+
+    println!("GUObjectArray = {guobjectarray:x?} FNamePool = {fnamepool:x?}");
+
+    let uobject_array = guobjectarray.read(&mem)?;
+
+    let mem = Ctx { mem, fnamepool };
+
+    let mut objects = BTreeMap::<String, ObjectType>::default();
+
+    for i in 0..uobject_array.ObjObjects.NumElements {
+        let obj_item = uobject_array.ObjObjects.read_item(&mem, i as usize)?;
+        let Some(obj) = obj_item.Object.read_opt(&mem)? else {
+            continue;
+        };
+        let Some(class) = obj.ClassPrivate.read_opt(&mem)? else {
+            continue;
+        };
+
+        let path = read_path(&mem, &obj)?;
+
+        fn read_struct(mem: &Ctx<impl Mem>, obj: &UStruct) -> Result<Struct> {
+            let mut properties = vec![];
+            let mut field = obj.ChildProperties;
+            while let Some(next) = field.read_opt(mem)? {
+                if let Some(prop) = map_prop(mem, field)? {
+                    properties.push(prop);
+                }
+                field = next.Next;
+            }
+            Ok(Struct { properties })
+        }
+
+        //if (path.starts_with("/Script/")) != !obj.ObjectFlags.contains(EObjectFlags::RF_WasLoaded) {
+        //    println!("{path:?} {:?}", obj.ObjectFlags);
+        //}
+
+        let f = class.ClassCastFlags;
+        if f.contains(EClassCastFlags::CASTCLASS_UClass) {
+            let full_obj = obj_item.Object.cast::<UClass>().read(&mem)?;
+            if full_obj.ClassFlags.contains(EClassFlags::CLASS_Native) {
+                objects.insert(
+                    path,
+                    ObjectType::Class(Class {
+                        r#struct: read_struct(&mem, &full_obj.ustruct)?,
+                    }),
+                );
+            }
+        } else if f.contains(EClassCastFlags::CASTCLASS_UFunction) {
+            let full_obj = obj_item.Object.cast::<UFunction>().read(&mem)?;
+            if full_obj.FunctionFlags.contains(EFunctionFlags::FUNC_Native) {
+                objects.insert(
+                    path,
+                    ObjectType::Function(Function {
+                        r#struct: read_struct(&mem, &full_obj.ustruct)?,
+                    }),
+                );
+            }
+        } else if f.contains(EClassCastFlags::CASTCLASS_UScriptStruct) {
+            let full_obj = obj_item.Object.cast::<UScriptStruct>().read(&mem)?;
+            if full_obj.StructFlags.contains(EStructFlags::STRUCT_Native) {
+                objects.insert(
+                    path,
+                    ObjectType::Struct(read_struct(&mem, &full_obj.ustruct)?),
+                );
+            }
+        } else if f.contains(EClassCastFlags::CASTCLASS_UEnum) {
+            let full_obj = obj_item.Object.cast::<UEnum>().read(&mem)?;
+            // TODO better way to determine native
+            if path.starts_with("/Script/") {
+                let mut names = vec![];
+                for TTuple { key, value } in full_obj.Names.read(&mem)? {
+                    names.push((mem.fnamepool.read(&mem, key)?, value));
+                }
+                objects.insert(
+                    path,
+                    ObjectType::Enum(Enum {
+                        cpp_type: full_obj.CppType.read(&mem)?,
+                        names,
+                    }),
+                );
+            }
+        } else if path.starts_with("/Script/") {
+            println!("{path:?} {:?}", f);
+        }
+    }
+    std::fs::write("fsd.json", serde_json::to_vec(&objects)?)?;
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_drg() -> Result<()> {
+        dump(1490227)
+    }
+}
---
+++ ue_reflection/src/lib.rs
@@ -0,0 +1,376 @@
+use serde::{Deserialize, Serialize};
+
+bitflags::bitflags! {
+    #[derive(Debug, Clone, Copy)]
+    #[repr(C)]
+    pub struct EObjectFlags: u32 {
+        const RF_NoFlags = 0x0000;
+        const RF_Public = 0x0001;
+        const RF_Standalone = 0x0002;
+        const RF_MarkAsNative = 0x0004;
+        const RF_Transactional = 0x0008;
+        const RF_ClassDefaultObject = 0x0010;
+        const RF_ArchetypeObject = 0x0020;
+        const RF_Transient = 0x0040;
+        const RF_MarkAsRootSet = 0x0080;
+        const RF_TagGarbageTemp = 0x0100;
+        const RF_NeedInitialization = 0x0200;
+        const RF_NeedLoad = 0x0400;
+        const RF_KeepForCooker = 0x0800;
+        const RF_NeedPostLoad = 0x1000;
+        const RF_NeedPostLoadSubobjects = 0x2000;
+        const RF_NewerVersionExists = 0x4000;
+        const RF_BeginDestroyed = 0x8000;
+        const RF_FinishDestroyed = 0x00010000;
+        const RF_BeingRegenerated = 0x00020000;
+        const RF_DefaultSubObject = 0x00040000;
+        const RF_WasLoaded = 0x00080000;
+        const RF_TextExportTransient = 0x00100000;
+        const RF_LoadCompleted = 0x00200000;
+        const RF_InheritableComponentTemplate = 0x00400000;
+        const RF_DuplicateTransient = 0x00800000;
+        const RF_StrongRefOnFrame = 0x01000000;
+        const RF_NonPIEDuplicateTransient = 0x02000000;
+        const RF_Dynamic = 0x04000000;
+        const RF_WillBeLoaded = 0x08000000;
+    }
+
+    #[derive(Debug, Clone, Copy)]
+    #[repr(C)]
+    pub struct EFunctionFlags: u32 {
+        const FUNC_None = 0x0000;
+        const FUNC_Final = 0x0001;
+        const FUNC_RequiredAPI = 0x0002;
+        const FUNC_BlueprintAuthorityOnly = 0x0004;
+        const FUNC_BlueprintCosmetic = 0x0008;
+        const FUNC_Net = 0x0040;
+        const FUNC_NetReliable = 0x0080;
+        const FUNC_NetRequest = 0x0100;
+        const FUNC_Exec = 0x0200;
+        const FUNC_Native = 0x0400;
+        const FUNC_Event = 0x0800;
+        const FUNC_NetResponse = 0x1000;
+        const FUNC_Static = 0x2000;
+        const FUNC_NetMulticast = 0x4000;
+        const FUNC_UbergraphFunction = 0x8000;
+        const FUNC_MulticastDelegate = 0x00010000;
+        const FUNC_Public = 0x00020000;
+        const FUNC_Private = 0x00040000;
+        const FUNC_Protected = 0x00080000;
+        const FUNC_Delegate = 0x00100000;
+        const FUNC_NetServer = 0x00200000;
+        const FUNC_HasOutParms = 0x00400000;
+        const FUNC_HasDefaults = 0x00800000;
+        const FUNC_NetClient = 0x01000000;
+        const FUNC_DLLImport = 0x02000000;
+        const FUNC_BlueprintCallable = 0x04000000;
+        const FUNC_BlueprintEvent = 0x08000000;
+        const FUNC_BlueprintPure = 0x10000000;
+        const FUNC_EditorOnly = 0x20000000;
+        const FUNC_Const = 0x40000000;
+        const FUNC_NetValidate = 0x80000000;
+        const FUNC_AllFlags = 0xffffffff;
+    }
+
+    #[derive(Debug, Clone, Copy)]
+    #[repr(C)]
+    pub struct EClassFlags: i32 {
+        const CLASS_None = 0x0000;
+        const CLASS_Abstract = 0x0001;
+        const CLASS_DefaultConfig = 0x0002;
+        const CLASS_Config = 0x0004;
+        const CLASS_Transient = 0x0008;
+        const CLASS_Parsed = 0x0010;
+        const CLASS_MatchedSerializers = 0x0020;
+        const CLASS_ProjectUserConfig = 0x0040;
+        const CLASS_Native = 0x0080;
+        const CLASS_NoExport = 0x0100;
+        const CLASS_NotPlaceable = 0x0200;
+        const CLASS_PerObjectConfig = 0x0400;
+        const CLASS_ReplicationDataIsSetUp = 0x0800;
+        const CLASS_EditInlineNew = 0x1000;
+        const CLASS_CollapseCategories = 0x2000;
+        const CLASS_Interface = 0x4000;
+        const CLASS_CustomConstructor = 0x8000;
+        const CLASS_Const = 0x00010000;
+        const CLASS_LayoutChanging = 0x00020000;
+        const CLASS_CompiledFromBlueprint = 0x00040000;
+        const CLASS_MinimalAPI = 0x00080000;
+        const CLASS_RequiredAPI = 0x00100000;
+        const CLASS_DefaultToInstanced = 0x00200000;
+        const CLASS_TokenStreamAssembled = 0x00400000;
+        const CLASS_HasInstancedReference = 0x00800000;
+        const CLASS_Hidden = 0x01000000;
+        const CLASS_Deprecated = 0x02000000;
+        const CLASS_HideDropDown = 0x04000000;
+        const CLASS_GlobalUserConfig = 0x08000000;
+        const CLASS_Intrinsic = 0x10000000;
+        const CLASS_Constructed = 0x20000000;
+        const CLASS_ConfigDoNotCheckDefaults = 0x40000000;
+        const CLASS_NewerVersionExists = i32::MIN;
+    }
+
+
+    #[derive(Debug, Clone, Copy)]
+    #[repr(C)]
+    pub struct EClassCastFlags : u64 {
+        const CASTCLASS_None = 0x0000000000000000;
+
+        const CASTCLASS_UField = 0x0000000000000001;
+        const CASTCLASS_FInt8Property = 0x0000000000000002;
+        const CASTCLASS_UEnum = 0x0000000000000004;
+        const CASTCLASS_UStruct = 0x0000000000000008;
+        const CASTCLASS_UScriptStruct = 0x0000000000000010;
+        const CASTCLASS_UClass = 0x0000000000000020;
+        const CASTCLASS_FByteProperty = 0x0000000000000040;
+        const CASTCLASS_FIntProperty = 0x0000000000000080;
+        const CASTCLASS_FFloatProperty = 0x0000000000000100;
+        const CASTCLASS_FUInt64Property = 0x0000000000000200;
+        const CASTCLASS_FClassProperty = 0x0000000000000400;
+        const CASTCLASS_FUInt32Property = 0x0000000000000800;
+        const CASTCLASS_FInterfaceProperty = 0x0000000000001000;
+        const CASTCLASS_FNameProperty = 0x0000000000002000;
+        const CASTCLASS_FStrProperty = 0x0000000000004000;
+        const CASTCLASS_FProperty = 0x0000000000008000;
+        const CASTCLASS_FObjectProperty = 0x0000000000010000;
+        const CASTCLASS_FBoolProperty = 0x0000000000020000;
+        const CASTCLASS_FUInt16Property = 0x0000000000040000;
+        const CASTCLASS_UFunction = 0x0000000000080000;
+        const CASTCLASS_FStructProperty = 0x0000000000100000;
+        const CASTCLASS_FArrayProperty = 0x0000000000200000;
+        const CASTCLASS_FInt64Property = 0x0000000000400000;
+        const CASTCLASS_FDelegateProperty = 0x0000000000800000;
+        const CASTCLASS_FNumericProperty = 0x0000000001000000;
+        const CASTCLASS_FMulticastDelegateProperty = 0x0000000002000000;
+        const CASTCLASS_FObjectPropertyBase = 0x0000000004000000;
+        const CASTCLASS_FWeakObjectProperty = 0x0000000008000000;
+        const CASTCLASS_FLazyObjectProperty = 0x0000000010000000;
+        const CASTCLASS_FSoftObjectProperty = 0x0000000020000000;
+        const CASTCLASS_FTextProperty = 0x0000000040000000;
+        const CASTCLASS_FInt16Property = 0x0000000080000000;
+        const CASTCLASS_FDoubleProperty = 0x0000000100000000;
+        const CASTCLASS_FSoftClassProperty = 0x0000000200000000;
+        const CASTCLASS_UPackage = 0x0000000400000000;
+        const CASTCLASS_ULevel = 0x0000000800000000;
+        const CASTCLASS_AActor = 0x0000001000000000;
+        const CASTCLASS_APlayerController = 0x0000002000000000;
+        const CASTCLASS_APawn = 0x0000004000000000;
+        const CASTCLASS_USceneComponent = 0x0000008000000000;
+        const CASTCLASS_UPrimitiveComponent = 0x0000010000000000;
+        const CASTCLASS_USkinnedMeshComponent = 0x0000020000000000;
+        const CASTCLASS_USkeletalMeshComponent = 0x0000040000000000;
+        const CASTCLASS_UBlueprint = 0x0000080000000000;
+        const CASTCLASS_UDelegateFunction = 0x0000100000000000;
+        const CASTCLASS_UStaticMeshComponent = 0x0000200000000000;
+        const CASTCLASS_FMapProperty = 0x0000400000000000;
+        const CASTCLASS_FSetProperty = 0x0000800000000000;
+        const CASTCLASS_FEnumProperty = 0x0001000000000000;
+        const CASTCLASS_USparseDelegateFunction = 0x0002000000000000;
+        const CASTCLASS_FMulticastInlineDelegateProperty = 0x0004000000000000;
+        const CASTCLASS_FMulticastSparseDelegateProperty = 0x0008000000000000;
+        const CASTCLASS_FFieldPathProperty = 0x0010000000000000;
+        const CASTCLASS_FLargeWorldCoordinatesRealProperty = 0x0080000000000000;
+        const CASTCLASS_FOptionalProperty = 0x0100000000000000;
+        const CASTCLASS_FVerseValueProperty = 0x0200000000000000;
+        const CASTCLASS_UVerseVMClass = 0x0400000000000000;
+    }
+
+    #[derive(Debug, Clone, Copy, Serialize, Deserialize)]
+    #[repr(C)]
+    pub struct  EPropertyFlags: u64 {
+        const CPF_None = 0x0000;
+        const CPF_Edit = 0x0001;
+        const CPF_ConstParm = 0x0002;
+        const CPF_BlueprintVisible = 0x0004;
+        const CPF_ExportObject = 0x0008;
+        const CPF_BlueprintReadOnly = 0x0010;
+        const CPF_Net = 0x0020;
+        const CPF_EditFixedSize = 0x0040;
+        const CPF_Parm = 0x0080;
+        const CPF_OutParm = 0x0100;
+        const CPF_ZeroConstructor = 0x0200;
+        const CPF_ReturnParm = 0x0400;
+        const CPF_DisableEditOnTemplate = 0x0800;
+        const CPF_Transient = 0x2000;
+        const CPF_Config = 0x4000;
+        const CPF_DisableEditOnInstance = 0x00010000;
+        const CPF_EditConst = 0x00020000;
+        const CPF_GlobalConfig = 0x00040000;
+        const CPF_InstancedReference = 0x00080000;
+        const CPF_DuplicateTransient = 0x00200000;
+        const CPF_SaveGame = 0x01000000;
+        const CPF_NoClear = 0x02000000;
+        const CPF_ReferenceParm = 0x08000000;
+        const CPF_BlueprintAssignable = 0x10000000;
+        const CPF_Deprecated = 0x20000000;
+        const CPF_IsPlainOldData = 0x40000000;
+        const CPF_RepSkip = 0x80000000;
+        const CPF_RepNotify = 0x100000000;
+        const CPF_Interp = 0x200000000;
+        const CPF_NonTransactional = 0x400000000;
+        const CPF_EditorOnly = 0x800000000;
+        const CPF_NoDestructor = 0x1000000000;
+        const CPF_AutoWeak = 0x4000000000;
+        const CPF_ContainsInstancedReference = 0x8000000000;
+        const CPF_AssetRegistrySearchable = 0x10000000000;
+        const CPF_SimpleDisplay = 0x20000000000;
+        const CPF_AdvancedDisplay = 0x40000000000;
+        const CPF_Protected = 0x80000000000;
+        const CPF_BlueprintCallable = 0x100000000000;
+        const CPF_BlueprintAuthorityOnly = 0x200000000000;
+        const CPF_TextExportTransient = 0x400000000000;
+        const CPF_NonPIEDuplicateTransient = 0x800000000000;
+        const CPF_ExposeOnSpawn = 0x1000000000000;
+        const CPF_PersistentInstance = 0x2000000000000;
+        const CPF_UObjectWrapper = 0x4000000000000;
+        const CPF_HasGetValueTypeHash = 0x8000000000000;
+        const CPF_NativeAccessSpecifierPublic = 0x10000000000000;
+        const CPF_NativeAccessSpecifierProtected = 0x20000000000000;
+        const CPF_NativeAccessSpecifierPrivate = 0x40000000000000;
+        const CPF_SkipSerialization = 0x80000000000000;
+    }
+
+    #[derive(Debug, Clone, Copy)]
+    #[repr(C)]
+    pub struct EInternalObjectFlags: u32 {
+        const None = 0x0;
+        const ReachableInCluster = 0x800000;
+        const ClusterRoot = 0x1000000;
+        const Native = 0x2000000;
+        const Async = 0x4000000;
+        const AsyncLoading = 0x8000000;
+        const Unreachable = 0x10000000;
+        const PendingKill = 0x20000000;
+        const RootSet = 0x40000000;
+        const GarbageCollectionKeepFlags = 0xe000000;
+        const AllFlags = 0x7f800000;
+    }
+
+    #[derive(Debug, Clone, Copy)]
+    #[repr(C)]
+    pub struct EStructFlags: i32 {
+        const STRUCT_NoFlags = 0x0000;
+        const STRUCT_Native = 0x0001;
+        const STRUCT_IdenticalNative = 0x0002;
+        const STRUCT_HasInstancedReference = 0x0004;
+        const STRUCT_NoExport = 0x0008;
+        const STRUCT_Atomic = 0x0010;
+        const STRUCT_Immutable = 0x0020;
+        const STRUCT_AddStructReferencedObjects = 0x0040;
+        const STRUCT_RequiredAPI = 0x0200;
+        const STRUCT_NetSerializeNative = 0x0400;
+        const STRUCT_SerializeNative = 0x0800;
+        const STRUCT_CopyNative = 0x1000;
+        const STRUCT_IsPlainOldData = 0x2000;
+        const STRUCT_NoDestructor = 0x4000;
+        const STRUCT_ZeroConstructor = 0x8000;
+        const STRUCT_ExportTextItemNative = 0x00010000;
+        const STRUCT_ImportTextItemNative = 0x00020000;
+        const STRUCT_PostSerializeNative = 0x00040000;
+        const STRUCT_SerializeFromMismatchedTag = 0x00080000;
+        const STRUCT_NetDeltaSerializeNative = 0x00100000;
+        const STRUCT_PostScriptConstruct = 0x00200000;
+        const STRUCT_NetSharedSerialization = 0x00400000;
+        const STRUCT_Trashed = 0x00800000;
+        const STRUCT_Inherit = 0x0014;
+        const STRUCT_ComputedFlags = 0x007ffc42;
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Struct {
+    pub properties: Vec<Property>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Class {
+    #[serde(flatten)]
+    pub r#struct: Struct,
+}
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Function {
+    #[serde(flatten)]
+    pub r#struct: Struct,
+}
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Enum {
+    pub cpp_type: String,
+    pub names: Vec<(String, i64)>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum ObjectType {
+    Struct(Struct),
+    Class(Class),
+    Function(Function),
+    Enum(Enum),
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Property {
+    pub name: String,
+    pub offset: usize,
+    pub size: usize,
+    pub r#type: PropertyType,
+    pub flags: EPropertyFlags,
+}
+#[derive(Debug, Serialize, Deserialize)]
+pub enum PropertyType {
+    Struct {
+        r#struct: String,
+    },
+    Str,
+    Name,
+    Text,
+    MulticastInlineDelegate,
+    MulticastSparseDelegate,
+    Delegate,
+    Bool {
+        field_size: u8,
+        byte_offset: u8,
+        byte_mask: u8,
+        field_mask: u8,
+    },
+    Array {
+        inner: Box<PropertyType>,
+    },
+    Enum {
+        container: Box<PropertyType>,
+        r#enum: String,
+    },
+    Map {
+        key_prop: Box<PropertyType>,
+        value_prop: Box<PropertyType>,
+    },
+    Set {
+        key_prop: Box<PropertyType>,
+    },
+    Float,
+    Double,
+    Byte {
+        r#enum: Option<String>,
+    },
+    UInt16,
+    UInt32,
+    UInt64,
+    Int8,
+    Int16,
+    Int,
+    Int64,
+    Object {
+        class: String,
+    },
+    WeakObject {
+        class: String,
+    },
+    SoftObject {
+        class: String,
+    },
+    LazyObject {
+        class: String,
+    },
+    Interface {
+        class: String,
+    },
+    FieldPath,
+}
